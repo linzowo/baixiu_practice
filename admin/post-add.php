c3d0146f1440f96d91aa4d9eb1fe6a030296be0a
linzowo <linzowo@outlook.com>
2019-07-24 23:29:33 +0800

    step-21-01:新增文章页面-动态获取文章分类数据

--- admin/post-add.php
+++ admin/post-add.php
@@ -1,9 +1,51 @@
-<?php 
+<!-- 引入依赖文件 -->
+<!-- 检查用户是否登录 -->
+<?php
 require_once '../function.php';
 bx_check_login_status();
 ?>
+<!-- ===================================== -->
+<!-- 动态获取数据库数据渲染页面 -->
+<?php
+// 分类信息
+$categories_arr = bx_get_db_data("SELECT * FROM categories;");
+?>
+<!-- ===================================== -->
+<!-- 声明数据处理函数 -->
+<?php
+/* array(6) {
+  ["title"]=&gt;
+  string(0) ""
+  ["content"]=&gt;
+  string(0) ""
+  ["slug"]=&gt;
+  string(0) ""
+  ["category"]=&gt;
+  string(1) "1"
+  ["created"]=&gt;
+  string(0) ""
+  ["status"]=&gt;
+  string(7) "drafted"
+}
+ */
+function post_add()
+{
+  global $msg;
+  $have_data = empty($_GET['title']) || empty($_GET['content']) || empty($_GET['slug']) || empty($_GET['category']) || empty($_GET['created']) || empty($_GET['status']);
+  if ($have_data) {
+    $msg = '请填写完整表单';
+    return;
+  }
+}
+?>
+<!-- ===================================== -->
+<!-- 调用数据处理函数 -->
+<?php
+post_add();
+?>
 <!DOCTYPE html>
 <html lang="zh-CN">
+
 <head>
   <meta charset="utf-8">
   <title>Add new post &laquo; Admin</title>
@@ -14,8 +56,11 @@ bx_check_login_status();
   <link rel="stylesheet" href="/static/assets/vendors/simplemde/simplemde.min.css">
   <script src="/static/assets/vendors/nprogress/nprogress.js"></script>
 </head>
+
 <body>
-  <script>NProgress.start()</script>
+  <script>
+    NProgress.start()
+  </script>
 
   <div class="main">
     <?php include 'inc/navbar.php'; ?>
@@ -24,10 +69,12 @@ bx_check_login_status();
         <h1>写文章</h1>
       </div>
       <!-- 有错误信息时展示 -->
-      <!-- <div class="alert alert-danger">
-        <strong>错误！</strong>发生XXX错误
-      </div> -->
-      <form class="row">
+      <?php if (!empty($msg)) : ?>
+        <div class="alert alert-danger">
+          <strong>错误！</strong>发生XXX错误
+        </div>
+      <?php endif; ?>
+      <form class="row" action="<?php $_SERVER['PHP_SELF']; ?>" method="post" enctype="multipart/form-data">
         <div class="col-md-9">
           <div class="form-group">
             <label for="title">标题</label>
@@ -53,8 +100,9 @@ bx_check_login_status();
           <div class="form-group">
             <label for="category">所属分类</label>
             <select id="category" class="form-control" name="category">
-              <option value="1">未分类</option>
-              <option value="2">潮生活</option>
+              <?php foreach ($categories_arr as $key => $value) : ?>
+                <option value="<?php echo $value['id']; ?>"><?php echo $value['name']; ?></option>
+              <?php endforeach; ?>
             </select>
           </div>
           <div class="form-group">
@@ -77,14 +125,19 @@ bx_check_login_status();
   </div>
 
   <?php $current_page = 'post-add'; ?>
-    <?php include 'inc/slider.php'; ?>
+  <?php include 'inc/slider.php'; ?>
 
   <script src="/static/assets/vendors/jquery/jquery.js"></script>
   <script src="/static/assets/vendors/bootstrap/js/bootstrap.js"></script>
   <script src="/static/assets/vendors/simplemde/simplemde.min.js"></script>
   <script>
-    var simplemde = new SimpleMDE({ element: $('#content')[0]});
+    var simplemde = new SimpleMDE({
+      element: $('#content')[0]
+    });
   </script>
-  <script>NProgress.done()</script>
+  <script>
+    NProgress.done()
+  </script>
 </body>
+
 </html>
