8af316b08366a87d54f2c560a9ac459d92267f0d
linzowo <linzowo@outlook.com>
2019-07-26 11:14:45 +0800

    step-23-01:新增文章页面-引入jQuery图片预览库

--- admin/post-add.php
+++ admin/post-add.php
@@ -30,16 +30,7 @@ $categories_arr = bx_get_db_data("SELECT * FROM categories;");
  */
 function post_add()
 {
-  global $msg
-  ,$categories_arr
-  ,$slug
-  ,$title
-  ,$feature
-  ,$created
-  ,$content
-  ,$status
-  ,$user_id
-  ,$category_id;
+  global $msg, $categories_arr, $slug, $title, $feature, $created, $content, $status, $user_id, $category_id;
   // 存储用户数据
   $slug = $_POST['slug'];
   $title = addslashes($_POST['title']);
@@ -55,7 +46,7 @@ function post_add()
     $msg = '请填写完整表单';
     return;
   }
-  
+
   // 校验文章标题===字数不能超过60==也就是20个字
   if (strlen($_POST['title']) > 60) {
     $msg = '标题过长';
@@ -123,9 +114,9 @@ function post_add()
   if (!$affected_rows) {
     $msg = '存入数据库失败';
     return;
-   }
+  }
   // TODO: 展示新增数据
-
+  header('Location: /admin/posts.php');
 }
 ?>
 <!-- ===================================== -->
@@ -174,42 +165,43 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         <div class="col-md-9">
           <div class="form-group">
             <label for="title">标题</label>
-            <input id="title" class="form-control input-lg" name="title" type="text" placeholder="文章标题"<?php echo empty($title)?'':" value='{$title}'"; ?>>
+            <input id="title" class="form-control input-lg" name="title" type="text" placeholder="文章标题" <?php echo empty($title) ? '' : " value='{$title}'"; ?>>
           </div>
           <div class="form-group">
             <label for="content">内容</label>
-            <textarea id="content" class="form-control input-lg" name="content" cols="30" rows="10" placeholder="内容"><?php echo empty($content)?'':stripslashes($content); ?></textarea>
+            <textarea id="content" class="form-control input-lg" name="content" cols="30" rows="10" placeholder="内容"><?php echo empty($content) ? '' : stripslashes($content); ?></textarea>
           </div>
         </div>
         <div class="col-md-3">
           <div class="form-group">
             <label for="slug">别名</label>
-            <input id="slug" class="form-control" name="slug" type="text" placeholder="slug"<?php echo empty($slug)?'':" value='{$slug}'"; ?>>
+            <input id="slug" class="form-control" name="slug" type="text" placeholder="slug" <?php echo empty($slug) ? '' : " value='{$slug}'"; ?>>
             <p class="help-block">https://zce.me/post/<strong>slug</strong></p>
           </div>
           <div class="form-group">
             <label for="feature">特色图像</label>
             <!-- show when image chose -->
-            <img class="help-block thumbnail" style="display: none">
-            <input id="feature" class="form-control" name="feature" type="file" accept="image/*">
+            <div class="help-block img-thumbnail"></div>
+            <!-- <img class="help-block thumbnail" style="display: none"> -->
+            <!-- <input id="feature" class="form-control" name="feature" type="file" accept="image/*"> -->
           </div>
           <div class="form-group">
             <label for="category">所属分类</label>
             <select id="category" class="form-control" name="category">
               <?php foreach ($categories_arr as $key => $value) : ?>
-                <option value="<?php echo $value['id']; ?>"<?php echo ($category_id === $value['id'])?' selected':''; ?>><?php echo $value['name']; ?></option>
+                <option value="<?php echo $value['id']; ?>" <?php echo ($category_id === $value['id']) ? ' selected' : ''; ?>><?php echo $value['name']; ?></option>
               <?php endforeach; ?>
             </select>
           </div>
           <div class="form-group">
             <label for="created">发布时间</label>
-            <input id="created" class="form-control" name="created" type="datetime-local"<?php echo empty($created)?'':" value='{$created}'"; ?>>
+            <input id="created" class="form-control" name="created" type="datetime-local" <?php echo empty($created) ? '' : " value='{$created}'"; ?>>
           </div>
           <div class="form-group">
             <label for="status">状态</label>
             <select id="status" class="form-control" name="status">
-              <option value="drafted"<?php echo $status === 'drafted'?' selected':''; ?>>草稿</option>
-              <option value="published"<?php echo $status === 'published'?' selected':''; ?>>已发布</option>
+              <option value="drafted" <?php echo $status === 'drafted' ? ' selected' : ''; ?>>草稿</option>
+              <option value="published" <?php echo $status === 'published' ? ' selected' : ''; ?>>已发布</option>
             </select>
           </div>
           <div class="form-group">
@@ -226,21 +218,33 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   <script src="/static/assets/vendors/jquery/jquery.js"></script>
   <script src="/static/assets/vendors/bootstrap/js/bootstrap.js"></script>
   <script src="/static/assets/vendors/simplemde/simplemde.min.js"></script> <!-- 引入富文本插件库 -->
+  <script src="/static/assets/vendors/upload/upload.js"></script><!-- 引入图片上传预览库 -->
   <script>
     // 创建一个富文本对象
     var simplemde = new SimpleMDE({
       element: $('#content')[0]
     });
     // TODO: 上传图片预览
-    $('#feature').on('change',function(){
-      var file = $(this).prop('files')[0];// 获取一个文件对象
-      var reader = new FileReader();
-      $(reader).on('load',function(evt){
-        $('.thumbnail').attr('src',evt.target.result).css({'width':'100px'}).fadeIn();
-      });
-      reader.readAsDataURL(file);
-    });
+    // $('#feature').on('change', function() {
+    //   var file = $(this).prop('files')[0]; // 获取一个文件对象
+    //   var reader = new FileReader();
+    //   $(reader).on('load', function(evt) {
+    //     $('.thumbnail').attr('src', evt.target.result).css({
+    //       'width': '100px'
+    //     }).fadeIn();
+    //   });
+    //   reader.readAsDataURL(file);
+    // });
     // TODO: 上传图片状态保持
+  </script>
+  <script type="text/javascript">
+    var dragImgUpload = new DragImgUpload(".img-thumbnail", {
+      callback: function(files) {
+        //回调函数，可以传递给后台等等
+        var file = files[0];
+        console.log(file.name);
+      }
+    })
   </script>
   <script>
     NProgress.done()
