21633e9cabca7e7161e875c220a11ce45eb59579
linzowo <linzowo@outlook.com>
2019-07-31 12:03:50 +0800

    step-28-01:评论页面-添加一个动态loading效果

--- admin/comments.php
+++ admin/comments.php
@@ -1,8 +1,8 @@
-<?php 
+<?php
 require_once '../function.php';
 bx_check_login_status();
 ?>
-<?php 
+<?php
 // TODO: 分页
 // TODO: 批准
 // TODO: 删除
@@ -10,6 +10,7 @@ bx_check_login_status();
 ?>
 <!DOCTYPE html>
 <html lang="zh-CN">
+
 <head>
   <meta charset="utf-8">
   <title>Comments &laquo; Admin</title>
@@ -17,10 +18,174 @@ bx_check_login_status();
   <link rel="stylesheet" href="/static/assets/vendors/font-awesome/css/font-awesome.css">
   <link rel="stylesheet" href="/static/assets/vendors/nprogress/nprogress.css">
   <link rel="stylesheet" href="/static/assets/css/admin.css">
+  <!-- 动态loading的css -->
+  <style type="text/css">
+    .ng-scope {
+      display: none;
+      position: fixed;
+      top: 0;
+      left: 0;
+      bottom: 0;
+      right: 0;
+      background-color: rgba(0, 0, 0, 0.5);
+      z-index: 999;
+      padding: 200px 50%;
+    }
+
+    @keyframes lds-spinner {
+      0% {
+        opacity: 1;
+      }
+
+      100% {
+        opacity: 0;
+      }
+    }
+
+    @-webkit-keyframes lds-spinner {
+      0% {
+        opacity: 1;
+      }
+
+      100% {
+        opacity: 0;
+      }
+    }
+
+
+    .lds-spinner {
+      position: relative;
+    }
+
+    .lds-spinner div {
+      left: 94px;
+      top: 48px;
+      position: absolute;
+      -webkit-animation: lds-spinner linear 1s infinite;
+      animation: lds-spinner linear 1s infinite;
+      background: #facd9e;
+      width: 12px;
+      height: 24px;
+      border-radius: 40%;
+      -webkit-transform-origin: 6px 52px;
+      transform-origin: 6px 52px;
+    }
+
+    .lds-spinner div:nth-child(1) {
+      -webkit-transform: rotate(0deg);
+      transform: rotate(0deg);
+      -webkit-animation-delay: -0.916666666666667s;
+      animation-delay: -0.916666666666667s;
+    }
+
+    .lds-spinner div:nth-child(2) {
+      -webkit-transform: rotate(30deg);
+      transform: rotate(30deg);
+      -webkit-animation-delay: -0.833333333333333s;
+      animation-delay: -0.833333333333333s;
+    }
+
+    .lds-spinner div:nth-child(3) {
+      -webkit-transform: rotate(60deg);
+      transform: rotate(60deg);
+      -webkit-animation-delay: -0.75s;
+      animation-delay: -0.75s;
+    }
+
+    .lds-spinner div:nth-child(4) {
+      -webkit-transform: rotate(90deg);
+      transform: rotate(90deg);
+      -webkit-animation-delay: -0.666666666666667s;
+      animation-delay: -0.666666666666667s;
+    }
+
+    .lds-spinner div:nth-child(5) {
+      -webkit-transform: rotate(120deg);
+      transform: rotate(120deg);
+      -webkit-animation-delay: -0.583333333333333s;
+      animation-delay: -0.583333333333333s;
+    }
+
+    .lds-spinner div:nth-child(6) {
+      -webkit-transform: rotate(150deg);
+      transform: rotate(150deg);
+      -webkit-animation-delay: -0.5s;
+      animation-delay: -0.5s;
+    }
+
+    .lds-spinner div:nth-child(7) {
+      -webkit-transform: rotate(180deg);
+      transform: rotate(180deg);
+      -webkit-animation-delay: -0.416666666666667s;
+      animation-delay: -0.416666666666667s;
+    }
+
+    .lds-spinner div:nth-child(8) {
+      -webkit-transform: rotate(210deg);
+      transform: rotate(210deg);
+      -webkit-animation-delay: -0.333333333333333s;
+      animation-delay: -0.333333333333333s;
+    }
+
+    .lds-spinner div:nth-child(9) {
+      -webkit-transform: rotate(240deg);
+      transform: rotate(240deg);
+      -webkit-animation-delay: -0.25s;
+      animation-delay: -0.25s;
+    }
+
+    .lds-spinner div:nth-child(10) {
+      -webkit-transform: rotate(270deg);
+      transform: rotate(270deg);
+      -webkit-animation-delay: -0.166666666666667s;
+      animation-delay: -0.166666666666667s;
+    }
+
+    .lds-spinner div:nth-child(11) {
+      -webkit-transform: rotate(300deg);
+      transform: rotate(300deg);
+      -webkit-animation-delay: -0.083333333333333s;
+      animation-delay: -0.083333333333333s;
+    }
+
+    .lds-spinner div:nth-child(12) {
+      -webkit-transform: rotate(330deg);
+      transform: rotate(330deg);
+      -webkit-animation-delay: 0s;
+      animation-delay: 0s;
+    }
+
+    .lds-spinner {
+      width: 200px !important;
+      height: 200px !important;
+      -webkit-transform: translate(-100px, -100px) scale(1) translate(100px, 100px);
+      transform: translate(-100px, -100px) scale(1) translate(100px, 100px);
+    }
+  </style>
   <script src="/static/assets/vendors/nprogress/nprogress.js"></script>
 </head>
+
 <body>
-  <script>NProgress.start()</script>
+  <!-- 动态loading图案 -->
+  <div class="lds-css ng-scope">
+    <div class="lds-spinner" style="width:100%;height:100%">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  </div>
+  <script>
+    NProgress.start()
+  </script>
 
   <div class="main">
     <?php include 'inc/navbar.php'; ?>
@@ -59,7 +224,7 @@ bx_check_login_status();
   </div>
 
   <?php $current_page = 'comments'; ?>
-    <?php include 'inc/slider.php'; ?>
+  <?php include 'inc/slider.php'; ?>
 
   <script src="/static/assets/vendors/jquery/jquery.js"></script>
   <script src="/static/assets/vendors/bootstrap/js/bootstrap.js"></script>
@@ -67,46 +232,66 @@ bx_check_login_status();
   <script src="/static/assets/vendors/twbs-pagination/jquery.twbsPagination.js"></script>
   <script id="comments_tmpl" type="text/x-jsrender">
     {{for comments}}
-      <tr class="{{:status === 'rejected'?'danger':status === 'held'?'warning': ''}}">
-        <td class="text-center"><input type="checkbox"></td>
-        <td class="text-center" width = "100">{{:author}}</td>
-        <td>{{:content}}</td>
-        <td style=" white-space:nowrap" >《{{:post_title}}》</td>
-        <td width = "100">{{:created}}</td>
-        <td class="text-center" width = "100">{{:status === 'rejected'?'拒绝':status === 'held'?'待审核': '通过'}}</td>
-        <td class="text-center" width = '150'>
-          {{if status === 'held'}}
-            <a href="post-add.html" class="btn btn-info btn-xs">批准</a>
-            <a class="btn btn-warning btn-xs btn-edit" href="javascript:;" data-status="rejected">拒绝</a>
-          {{else}}
-            <a href="javascript:;" class="btn btn-danger btn-xs">删除</a>
+    <tr class="{{:status === 'rejected'?'danger':status === 'held'?'warning': ''}}">
+      <td class="text-center"><input type="checkbox"></td>
+      <td class="text-center" width="100">{{:author}}</td>
+      <td>{{:content}}</td>
+      <td style=" white-space:nowrap">《{{:post_title}}》</td>
+      <td width="100">{{:created}}</td>
+      <td class="text-center" width="100">{{:status === 'rejected'?'拒绝':status === 'held'?'待审核': '通过'}}</td>
+      <td class="text-center" width='150'>
+        {{if status === 'held'}}
+        <a href="post-add.html" class="btn btn-info btn-xs">批准</a>
+        <a class="btn btn-warning btn-xs btn-edit" href="javascript:;" data-status="rejected">拒绝</a>
+        {{else}}
+          <a href="javascript:;" class="btn btn-danger btn-xs">删除</a>
           {{/if}}
-        </td>
-      </tr>
+      </td>
+    </tr>
     {{/for}}
   </script>
   <script>
-   /**
-    * 获取分页数据并渲染
-    * @param int page 查询第几页的数据
-    */
-    function loadPageDate(page){
+    // loading特效
+    $(document)
+      .ajaxStart(function() {
+        NProgress.start()
+        // 加载loading图像
+        $('.lds-css').fadeIn();
+      })
+      .ajaxStop(function() {
+        NProgress.done()
+        // 关闭loading图像
+        $('.lds-css').fadeOut();
+      })
+    /**
+     * 获取分页数据并渲染
+     * @param int page 查询第几页的数据
+     */
+    function loadPageDate(page) {
       // 获取元素
       var tbody = $('tbody');
       // 通过ajax获取数据
       tbody.fadeOut();
-      $.get('/admin/api/comments.php',{ page:page },function(res){
+      $.get('/admin/api/comments.php', {
+        page: page
+      }, function(res) {
         // 使用jquery分页库生成分页数据
         $('.pagination').twbsPagination({
           totalPages: res['total_page'],
           visiblePages: 5,
+          first: '&laquo;',
+          last: '&raquo;',
+          prev: '&lt;',
+          next: '&gt;',
           initiateStartPageClick: false,
-          onPageClick: function(e,page){
+          onPageClick: function(e, page) {
             loadPageDate(page);
           }
         });
         // 获取当前页面应该展示的数据并渲染
-        var tr = $('#comments_tmpl').render({ comments:res['comments'] });
+        var tr = $('#comments_tmpl').render({
+          comments: res['comments']
+        });
         $('tbody').html(tr);
         tbody.fadeIn();
       });
@@ -114,6 +299,9 @@ bx_check_login_status();
     // 初始化页面
     loadPageDate(1);
   </script>
-  <script>NProgress.done()</script>
+  <script>
+    NProgress.done()
+  </script>
 </body>
+
 </html>
